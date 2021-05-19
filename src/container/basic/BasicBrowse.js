88734925f0b7a7772ab6ce135722f6b6bf119cb3
Ocean-0 <ocean_zer0@163.com>
2021-05-19 09:44:35 +0800

    更新

--- src/container/basic/BasicBrowse.js
+++ src/container/basic/BasicBrowse.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {observer,inject,Provider} from 'mobx-react'
 import {itemName} from '../../static/ItemName'
 import {Item} from '../../static/Item'
+import '../../CSS/Browse.css'
 
 
 @observer
@@ -32,16 +33,55 @@ class BasicBrowse extends Component{
         });
     }
     render(){
-        const basic = itemName.basic;
-        const jobinfo = itemName.jobinfo;
-        const workin = itemName.workin;
-        const workout = itemName.workout;
-        const eduinfo = itemName.eduinfo;
-        const member = itemName.member;
-        const skill = itemName.skill;
+        const {basic,jobinfo,workin,workout,eduinfo,member,skill} = itemName;
+        const {p,b} = this.state;
         return (
-            <div id="browse-container">BasicBrowse
-                
+            <div id="browse-container">
+                <div className="browse-g">
+                    <span>基础信息</span>
+                    <span>设置</span>
+                    <div className="line"></div>
+                    <div className="browse-content">
+                        <div>{basic[0]}:</div><div>{basic[1]}:</div><div>{basic[2]}:</div>
+                        <div>{basic[3]}:</div><div>{basic[4]}:</div><div>{basic[5]}:</div>
+                        <div>{basic[6]}:</div><div>{basic[7]}:</div><div>{basic[8]}:</div>
+                        <div>{basic[9]}:</div><div>{basic[10]}:</div><div>{basic[11]}:</div>
+                        <div>{basic[12]}:</div><div>{basic[13]}:</div><div>{basic[14]}:</div>
+                        <div>{basic[15]}:</div><div>{basic[16]}:</div><div>{basic[17]}:</div>
+                        <div>{basic[18]}:</div><div>{basic[19]}:</div><div>{basic[20]}:</div>
+                        <div>{basic[21]}:</div>
+                    </div>
+                </div>
+                <div className="browse-g">
+                    <span>职务信息</span>
+                    <span>设置</span>
+                    <div className="line"></div>
+                    <div className="browse-content">
+                        <div>{jobinfo[0]}:</div> <div>{jobinfo[1]}:</div> <div>{jobinfo[2]}:</div>
+                        <div>{jobinfo[3]}:</div> <div>{jobinfo[4]}:</div> <div>{jobinfo[5]}:</div>
+                        <div>{jobinfo[6]}:</div> <div>{jobinfo[7]}:</div> <div>{jobinfo[8]}:</div>
+                    </div>
+                </div>
+                <div className="browse-g">
+                    <span>内部工作经历</span>
+                    <span>设置</span>
+                    <div className="line"></div>
+                    <div className="browse-content">
+                        <div>{workin[0]}:</div> <div>{workin[1]}:</div> <div>{workin[2]}:</div>
+                        <div>{workin[3]}:</div>
+                    </div>
+                </div>
+                <div className="browse-g">
+                    <span>外部工作经历</span>
+                    <span>设置</span>
+                    <div className="line"></div>
+                    <div className="browse-content">
+                        <div>{workout[0]}:</div> <div>{workout[1]}:</div> <div>{workout[2]}:</div>
+                        <div>{workout[3]}:</div> <div>{workout[4]}:</div>
+                    </div>
+                </div>
+
+
             </div>
         )
     }
