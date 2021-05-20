3e61fc73e75f9123931f3b0ebb3ba368c48bc8ce
Ocean-0 <ocean_zer0@163.com>
2021-05-20 17:15:29 +0800

    更新

--- src/container/attendance/AttBrowse.js
+++ src/container/attendance/AttBrowse.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import * as echarts from 'echarts'
-import {reqAnSex,reqAnAbility,reqAnEdu} from '../../public/api/ReqApi'
+import {reqAttCount} from '../../public/api/ReqApi'
 import {observer,inject,Provider} from 'mobx-react'
 import Qs from 'qs'
 import '../../CSS/Attendance.css'
@@ -17,18 +17,35 @@ class AttBrowse extends Component{
         }
     }
     componentDidMount(){
+        console.log(this.props.dataContainer.loginAcc)
+        reqAttCount(Qs.stringify({
+            account: this.props.dataContainer.loginAcc,
+            token: 'home login'+Math.random()
+        }), 'POST')
+        .then((res) => {
+            console.log('att did mount：',res.data)
+            this.setState({
+                data:res.data,
+                error:''
+            })
+            option.series.data = res.data;
+        })
+        .catch((e) => {
+            this.setState({error:'网络错误,请重试'});
+            console.log('网络错误,请重试', e)
+        });
+
 
         var chartDom = document.getElementById('timeMap');
         var myChart = echarts.init(chartDom);
         myChart.setOption(option)
-
 
     }
     render(){
         return (
             // id => class 组件空间width将占据页面
             <div id="att-container">
-                <div className="att-g">
+                <div className="att-g att-header">
                     <dvi id="timeMap">#</dvi>
                 </div>
             </div>
@@ -42,7 +59,7 @@ const option = {
     title: {
         top: 30,
         left: 'center',
-        text: '2021年每日工作时长'
+        text: '2021年每日工作时长（小时）'
     },
     tooltip: {},
     visualMap: {
--- src/public/api/ReqApi.js
+++ src/public/api/ReqApi.js
@@ -9,6 +9,7 @@ const PATH_BASICDATA_01 = 'getBasicData';
 const PATH_ANSEX_01 = 'getAnSex';
 const PATH_ANAILITY_01 = 'getAnAbiliy';
 const PATH_ANEDU_01 = 'getAnEdu';
+const PATH_ATTCOUNT_01 = 'getAttCount';
 
 export const reqLogin = (data, method) => send(URL_JOBLIST, PATH_LOGIN_01, data, method)
 export const reqJobList = (data, method) => send(URL_JOBLIST, PATH_JOBLIST_01, data, method)
@@ -17,3 +18,4 @@ export const reqBasic = (data, method) => send(URL_JOBLIST, PATH_BASICDATA_01, d
 export const reqAnSex = (data, method) => send(URL_JOBLIST, PATH_ANSEX_01, data, method)
 export const reqAnAbility = (data, method) => send(URL_JOBLIST, PATH_ANAILITY_01, data, method)
 export const reqAnEdu = (data, method) => send(URL_JOBLIST, PATH_ANEDU_01, data, method)
+export const reqAttCount = (data, method) => send(URL_JOBLIST, PATH_ATTCOUNT_01, data, method)
